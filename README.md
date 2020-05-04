f0be1f51ac5e4b3d9e351e1abf138d9e5e28978d
nanileb <47608919+nanileb@users.noreply.github.com>
2020-05-04 22:21:21 +0200

    Revert "New Version"
    This reverts commit 4b22677f04a264ac6c54b0ec06627f9cf0df2c7d.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Mailbox
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.4.
 
 ## Development server
 
---
+++ src/service-worker.js
@@ -0,0 +1,37 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('ScanTool').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      caches.open("ScanTool").then(function(cache) {
+        cache.match(event.request).then(function (){
+              cache.put(event.request, response.clone());
+              console.log("cache updated");
+        });
+      });
+      console.log("Response from network");
+      return response.clone();
+    }).catch(function (){
+      return caches.match(event.request).then(function(response) {
+        if(response){
+          console.log("Response from cache");
+          return response;
+        } else {
+          fetch(event.request.clone());
+        }
+      });
+    })
+  )
+});
+
+
